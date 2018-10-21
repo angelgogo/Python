fce72c6f265721e518cbac9651d5f9dedc461c13
xiehui <xiehuister@hotmail.com>
2018-10-21 11:35:20 +0800

    Merge remote-tracking branch 'remotes/origin/master'
    # Conflicts:
    #	Day1/123123123.py

--- DAY4/zhuangshiqi.py
+++ DAY4/zhuangshiqi.py
@@ -1,6 +1,29 @@
-# -*- coding:utf-8 -*-
-def logger():
-    print('This is a test Log!')
+import os
 
 
-int()
+def logger(ff):
+    def fu():
+        print("This is a Logger,修饰器！")
+        ff()
+
+    return fu
+
+
+@logger
+def tes1():
+    print("This is the test1")
+
+
+@logger
+def test2():
+    print("This is the test2")
+
+
+pathnow=os.path
+
+print('5555555555555555', pathnow)
+tes1()
+
+tes1()
+
+test2()
